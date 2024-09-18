9fee2ec30a656f4d7dc9aa0662b6fd92eb8a71d8
Tiras Mwangi <tmwangi599@gmail.com>
2024-09-18 12:39:47 +0300

    view and update profile views

---
+++ resources/views/view-profile.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            {{ __('Profile') }}
+        </h2>
+    </x-slot>
+
+    <livewire:components.view-profile :$user />
+</x-app-layout>
