04d083df1d30c2b2d622f8c3fefbcc608d30ef8f
Tiras Mwangi <tmwangi599@gmail.com>
2024-09-23 13:47:33 +0300

    checks whether a freelancer has made a bid to an assignment. Restricts a freelancer from making multiple bids on an assignment.

--- app/Http/Controllers/BidController.php
+++ app/Http/Controllers/BidController.php
@@ -2,15 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Assignment;
 use App\Models\Bid;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\View\View;
+use App\Models\Assignment;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 
 class BidController extends Controller
 {
-    public function create(Assignment $assignment): View
+    public function create(Assignment $assignment): View | RedirectREsponse
     {
+        $userId = Auth::user()->id;
+
+        $bid = $assignment->bids()
+                ->where('user_id', $userId)
+                ->first();
+
+        if ($bid) {
+            return redirect()->route('bids.show', ['bid' => $bid->id]);
+        }
+
         return view('bids.create', [
             'assignment' => $assignment,
         ]);
